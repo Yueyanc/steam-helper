44829656523db89f1b9fd3a69530422ff708323c
AdienChen <565302968@qq.com>
2023-12-03 22:07:09 +0800

    feat: 优化代码和样式

--- utils/plugins/auto-import-css.ts
+++ utils/plugins/auto-import-css.ts
@@ -13,20 +13,5 @@ function getInjectionCode(fileName: string): string {
 export default function AutoImportCss({ cssKey = "" }: Config): PluginOption {
   return {
     name: "auto-import-css",
-    load(id) {
-      if (id.includes("/pages/content/index.ts")) {
-        return (
-          getInjectionCode(id) +
-          "\n" +
-          "var link = document.createElement('link');" +
-          "link.href = chrome.runtime.getURL(" +
-          `'assets/css/contentStyle${cssKey}.chunk.css'` +
-          ");" +
-          "link.type = 'text/css';" +
-          "link.rel = 'stylesheet';" +
-          "document.getElementsByTagName('head')[0].appendChild(link);"
-        );
-      }
-    },
   };
 }
